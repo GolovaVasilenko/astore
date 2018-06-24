90bcfb6eafe16d6258ea6b43d221e515bcf87eca
GolovaVasilenko <vasilenko75@gmail.com>
2018-06-24 20:34:10 +0300

    create init file and constant configurate

---
+++ config/init.php
@@ -0,0 +1,27 @@
+<?php
+
+define("DEBUG", 1);
+
+define("ROOT", dirname(__DIR__));
+
+define("WWW", ROOT . "/public");
+
+define("APP", ROOT . "/app");
+
+define("CORE", ROOT . "/vendor/astore/core");
+
+define("LIBS", ROOT . "/vendor/astore/core/libs");
+
+define("CACHE", ROOT . "/tmp/cache");
+
+define("CONF", ROOT . "/config");
+
+define("LAYOUT", ROOT . "default");
+
+$protocol = "http://";
+
+define("PATH", $protocol . $_SERVER["HTTP_HOST"] . "/");
+
+define("ADMIN", PATH . "admin/");
+
+require_once ROOT . "vendor/autoload.php";
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: alexey
- * Date: 24.06.18
- * Time: 17:08
- */
-echo "It's Work";
