11da50e89ced1dee5f3b534828828ca1c9928928
GolovaVasilenko <vasilenko75@gmail.com>
2018-07-28 20:14:27 +0300

    last commit

--- app/models/ProductModel.php
+++ app/models/ProductModel.php
@@ -3,13 +3,39 @@
 namespace app\models;
 
 
+use astore\App;
+
 class ProductModel extends AppModel
 {
+    const TABLE = "products";
+
+    public $title;
+
+    public $slug;
+
+    public $description;
+
+    public $excerpt;
+
+    public $brand_id;
+
+    public $meta_title;
+
+    public $meta_desc;
+
+    public $h1;
+
+    public $active;
+
+    public $hit;
+
+    public $recommended;
+
+    public $position;
+
     protected $categories = [];
 
     protected $brand;
-
-    const TABLE = "products";
     
 
     public static function getHitProducts()
@@ -33,9 +59,26 @@ class ProductModel extends AppModel
 
     public function __get($key)
     {
-        if(!isset($this->relations[$key]))
-            $this->relations[$key] = \R::find($key, 'WHERE product_id = ?', $this->attributes['id']);
-        return $this->relations[$key];
+        if(property_exists($this, $key))
+            $db = App::$app->getProperty('db');
+            switch($key){
+                case 'categories':
+                    $query = "SELECT * FROM " . $key . " AS cat 
+                              JOIN categories_products AS cp ON cat.id = cp.categories_id 
+                              JOIN products AS p ON cp.products_id = p.id 
+                              WHERE p.id = :id";
+                    $result = $db->query($query, CategoryModel::class, ['id' => $this->id]);
+                    break;
+                case 'brands':
+                    $query = "SELECT * FROM " . $key . " AS b WHERE b.id = :id";
+                    $result = $db->query($query, BrandModel::class, ['id' => $this->brand_id]);
+                    break;
+                default:
+                    parent::__get($key);
+                    break;
+            }
+            $this->$key = \R::find($key, 'WHERE product_id = ?', $this->id);
+        return $this->$key;
     }
 
     public static function countProduct()
